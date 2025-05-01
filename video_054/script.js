606c5cbc33463d9527843383fad9313a4bfa84e7
Ather Saeed <athers896@gmail.com>
2025-05-01 03:30:57 -0400

    video_054: JS Introduction

---
+++ video_054/script.js
@@ -0,0 +1,14 @@
+alert("Hello World");
+
+console.log("Code is running...")
+
+var a= prompt("Enter your age: ")
+var isTrue = confirm("Are you sure?")
+
+console.log(a)
+
+if (isTrue){
+    console.log("oki doki")
+}else{
+    console.log("oki doki")
+}
