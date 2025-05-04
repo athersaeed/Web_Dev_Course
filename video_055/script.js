fa660f2565fa24e983f600a8b398d316fc6d2a00
Ather Saeed <athers896@gmail.com>
2025-05-03 22:05:52 -0400

    video_055: JS Variables

---
+++ video_055/script.js
@@ -0,0 +1,39 @@
+console.log("hey this is tuts 55")
+
+let a = 5;
+let b = 6;
+let c = "ather"
+
+console.log(a + b)
+
+console.log(typeof a, typeof b, typeof c)
+
+{
+    let a = 66;
+    console.log(a)
+}
+
+console.log(a)
+// output: 5
+
+// if a was var then both console.logs would print 66
+
+let x = "ather bhai"
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined
+let r = null
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name: "ather",
+    "job code": 5600
+}
+
+console.log(o)
+
+o.salary = "100crores"
+console.log(o)
