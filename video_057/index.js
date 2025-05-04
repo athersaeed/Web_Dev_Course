e390f5f3b7809c365adbf1128bbfa260e205bc40
Ather Saeed <athers896@gmail.com>
2025-05-04 11:00:04 -0400

    video_057: JS Loops

---
+++ video_057/index.js
@@ -0,0 +1,38 @@
+console.log("Hi I am a tuts on JS Loops");
+
+let a = 1
+
+// for (let i = 0; i < 100; i++){
+//     console.log(a+i);
+// }
+
+// let obj = {
+//     name: "ather",
+//     role: "Programmer", 
+//     company: "CodeWithAther"
+// }
+
+// for (const key in obj) {
+//     if (Object.prototype.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key, element);
+        
+//     }
+// }
+
+// for (const element of "object") {
+//     console.log(element);   
+// }
+
+// let i = 0
+// while (i < 6) {
+//     console.log(i);
+//     i++;    
+// }
+
+
+let i = 10
+do {
+    console.log(i);
+    i++;
+} while (i<6);
