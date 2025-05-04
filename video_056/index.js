f13fb91a1081be89eb57630d0f93b832ee73ae97
Ather Saeed <athers896@gmail.com>
2025-05-04 00:31:18 -0400

    video_056: Conditionals

---
+++ video_056/index.js
@@ -0,0 +1,11 @@
+console.log("Hello i am Conditional Tutorial")
+
+let age = 16;
+let grace = 2;
+
+if (age+grace >= 18){
+    console.log("you can drive")
+}else{
+    console.log("you cannot drive");
+}
+
