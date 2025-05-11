fadba803c40b3709ff89033d53170737e4e4d7e6
Ather Saeed <athers896@gmail.com>
2025-05-11 00:30:40 -0400

    video_067: JS DOM

---
+++ video_067/script.js
@@ -0,0 +1,18 @@
+console.log("Hello World!")
+
+let cont = document.body.childNodes[1]
+
+cont.firstElementChild.style.color = "blue"
+
+cont.firstElementChild.style.backgroundColor = "red"
+
+
+cont.firstElementChild.parentElement
+
+document.body.firstElementChild
+
+document.body.firstElementChild.children
+
+document.body.firstElementChild.children[3].nextElementSibling
+
+document.body.children[1]
